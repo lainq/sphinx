ea1e228dfebd596862773aa5564f359c8db4116b
Pranav <70764593+pranavbaburaj@users.noreply.github.com>
2021-04-02 14:11:23 +0530

    Formatted the code

--- index.ts
+++ index.ts
@@ -1,59 +1,66 @@
-import { config } from 'dotenv'
-import { Client, Message } from 'discord.js'
-import { Fcal, FcalError } from 'fcal'
+import {config} from 'dotenv';
+import {Client, Message} from 'discord.js';
+import {Fcal, FcalError} from 'fcal';
+import {SphinxException} from './src/error';
 
 // Take all the variables from the env
-// file to process.env 
-config()
+// file to process.env
+config();
 
 // constants
-const token = process.env.TOKEN
-const prefix:Array<string> = ["=", "!", ";"]
+const token = process.env.TOKEN;
+const prefix: Array<string> = ['=', '!', ';', 'sphinx'];
 
 // the discord clinet
-const client = new Client()
+const client = new Client();
 
 /**
  * Checks whether the  message is a commandor not
- * 
+ *
  * @param {String} message The message content to check if a command
  * @returns An object with type and command
  */
-const isBotCommand = (message:string):any => {
-    for(let index=0; index<prefix.length; index++){
-        if(message.startsWith(prefix[index])){
-            return {
-                type : prefix[index],
-                command : true
-            }
-        }
+const isBotCommand = (message: string): any => {
+  for (let index = 0; index < prefix.length; index++) {
+    if (message.toLowerCase().startsWith(prefix[index])) {
+      return {
+        type: prefix[index],
+        command: true,
+      };
     }
-    return {type : null, command : false}
-}
+  }
+  return {type: null, command: false};
+};
 
-client.on("ready", () => { 
-    console.log("The bot has started");
+client.on('ready', () => {
+  console.log('The bot has started');
 });
 
-client.on("message", (message: Message) => { 
-    const command:any = isBotCommand(message.content)
-    if(command.command){
-        if(command.type == "="){
-            const calculations = message.content.slice(1, message.content.length)
-            try {
-                const data = Fcal.eval(calculations)
-                message.reply(data.toString())
-            } catch(exception){
-                if(exception instanceof FcalError){
-                    message.reply(exception.info())
-                }
-            }
+client.on('message', async (message: Message) => {
+  const command: any = isBotCommand(message.content);
+  if (command.command) {
+    if (command.type == '=') {
+      const calculations = message.content.slice(1, message.content.length);
+      try {
+        const data = Fcal.eval(calculations);
+        message.reply(data.toString());
+      } catch (exception) {
+        if (exception instanceof FcalError) {
+          const error = new SphinxException(
+            'An error occured while parsing your message :frowning:',
+            message
+          );
+          error.evokeSphinxException();
         }
+      }
+    } else if (command.type == 'sphinx') {
+      message.channel.send('Hi');
     }
+  }
 });
 
-client.on("error", e => {
-    console.error("Discord client error!", e);
- });
+client.on('error', (e) => {
+  console.error('Discord client error!', e);
+});
 
 client.login(token);
--- src/error.ts
+++ src/error.ts
@@ -2,26 +2,23 @@ import {MessageEmbed, Message} from 'discord.js';
 
 export class SphinxException {
   private readonly message: string;
-  private messageChannel:Message
+  private messageChannel: Message;
 
-  constructor(readonly messageData: string, message:Message) {
+  constructor(readonly messageData: string, message: Message) {
     this.message = messageData;
-    this.messageChannel = message
+    this.messageChannel = message;
   }
 
   public evokeSphinxException = () => {
     const embed = new MessageEmbed()
       .setTitle('An unexpected error occured')
       .setAuthor('Code Roller', 'https://i.imgur.com/lm8s41J.png')
-      .setColor("#CC0000")
-      .setDescription(
-        this.message
-      )
-      .setImage('http://i.imgur.com/yVpymuV.png')
+      .setColor('#CC0000')
+      .setDescription(this.message)
       .setThumbnail('http://i.imgur.com/p2qNFag.png')
       .setTimestamp()
-      .setURL('')
-      
-    this.messageChannel.channel.send(embed)
+      .setURL('');
+
+    this.messageChannel.channel.send(embed);
   };
 }
