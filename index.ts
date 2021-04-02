1194760de0522ec9e2c227435157ebcc1d5fffc0
Pranav <70764593+pranavbaburaj@users.noreply.github.com>
2021-04-02 11:21:39 +0530

    Added basic calculations

---
+++ index.ts
@@ -0,0 +1,48 @@
+import { config } from 'dotenv'
+import { Client, Message } from 'discord.js'
+
+// Take all the variables from the env
+// file to process.env 
+config()
+
+const token = process.env.TOKEN
+const prefix:Array<string> = ["=", "!", ";"]
+
+// the discord clinet
+const client = new Client()
+
+const isBotCommand = (message:string):any => {
+    for(let index=0; index<prefix.length; index++){
+        if(message.startsWith(prefix[index])){
+            return {
+                type : prefix[index],
+                command : true
+            }
+        }
+    }
+    return {type : null, command : false}
+}
+
+client.on("ready", () => { 
+    console.log("The bot has started");
+});
+
+client.on("message", (message: Message) => { 
+    const command:any = isBotCommand(message.content)
+    if(command.command){
+        if(command.type == "="){
+            const regex = /(?:(?:^|[-+_*/])(?:\s*-?\d+(\.\d+)?(?:[eE][+-]?\d+)?\s*))+$/
+            const calculations = message.content.slice(1, message.content.length)
+            const isEquation = regex.test(calculations)
+            if(isEquation){
+                message.reply(eval(calculations))
+            }
+        }
+    }
+});
+
+client.on("error", e => {
+    console.error("Discord client error!", e);
+ });
+
+client.login(token);
