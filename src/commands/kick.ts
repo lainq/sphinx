639ca2b0a0b037377522b36792d20c2281e71efb
Pranav <70764593+pranavbaburaj@users.noreply.github.com>
2021-04-03 11:25:50 +0530

    Finished kick command

--- src/commands/kick.ts
+++ src/commands/kick.ts
@@ -1,4 +1,4 @@
-import { GuildMember, Message } from 'discord.js'
+import { Guild, GuildMember, Message } from 'discord.js'
 import { SphinxException } from '../error';
 
 export class SphinxKickCommand {
@@ -13,8 +13,29 @@ export class SphinxKickCommand {
     public kickMember = () => {
         if(this.mentionCount > 0){
             const kickMembers = this.message.mentions.members
+            if(!this.message.member?.hasPermission("KICK_MEMBERS")){
+                const exception = new SphinxException(
+                    "You don't have permissions to kick anyone",
+                    this.message
+                ).evokeSphinxException()
+                return null
+            }
             kickMembers?.every((value:GuildMember) => {
-                console.log(value)
+                try{
+                    value.kick("Just for fun").then((member:GuildMember) => {
+                        this.message.channel.send(`Kicked ${member.displayName}`)
+                    }).catch((error) => {
+                        const exception = new SphinxException(
+                            `I don't have permissions to kick ${value.displayName}`,
+                            this.message
+                        ).evokeSphinxException()
+                    })
+                } catch(error) {
+                    const exception = new SphinxException(
+                        `I do not have permissions to kick ${value.displayName}`,
+                        this.message
+                    ).evokeSphinxException()
+                }
                 return true
             })
         } else {
