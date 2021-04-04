41e1d43d6c7759c38202b31e6766c290affbbfc3
Pranav <70764593+pranavbaburaj@users.noreply.github.com>
2021-04-04 15:34:41 +0530

    Check for message duplication

--- src/duplicate.ts
+++ src/duplicate.ts
@@ -1,8 +1,9 @@
 import { Guild, GuildMember, Message, User } from "discord.js";
+import { SphinxException } from "./error";
 
 class SphinxDuplicateMessage {
     private message:Message
-    private author:GuildMember | undefined | User
+    private author:undefined | User
     private server:Guild | null
 
     constructor(message:Message) {
@@ -12,14 +13,34 @@ class SphinxDuplicateMessage {
             (value:GuildMember) => {
                 return value.user.username == this.message.author.username
             }
-        ).first()
+        ).first()?.user
 
-        console.log(this.author)
+    }
+
+    public findDuplicateMessage = async () => {
+        const messages = await this.message.channel.messages.fetch({limit:10})
+        let userMessages = Array.from(messages.filter((message:Message) => {
+            if(message.author.bot){
+                return false
+            }
+            return (
+                message.author.username == this.message.author.username &&
+                message.content.toLowerCase() == this.message.content.toLowerCase()
+            )
+        }).keys())
+        if((userMessages.length-1) >= 3) {
+            this.message.delete()
+            const exception = new SphinxException(
+                `${this.author?.tag} repeated a message 3 times. The message has been deleted`,
+                this.message
+            ).evokeSphinxException()
+        }
+        return false
     }
 }
 
 export const isDuplicateMessage = (message:Message):Boolean => {
-    const checkDuplicateMessage = new SphinxDuplicateMessage(message)
-    return false   
+    const checkDuplicateMessage = new SphinxDuplicateMessage(message).findDuplicateMessage()
+    return false
 }
 
