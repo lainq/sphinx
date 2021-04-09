a781c38a338dc29887430f074d93df3b87212349
Pranav <70764593+pranavbaburaj@users.noreply.github.com>
2021-04-09 13:15:52 +0530

    Rewrite the store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,13 +1,14 @@
+import { existsSync, readFileSync, writeFile } from "fs"
 
 export interface DataStore {
     databaseName : string,
-    databasePath : string | null,
+    databasePath : string
     exists : boolean
 }
 
 export class SphinxDataStore {
     private readonly database:string
-    private path: string | null
+    private path: string
     private exists:boolean
 
     private data:string = ""
@@ -17,14 +18,47 @@ export class SphinxDataStore {
         this.path = data.databasePath
         this.exists = data.exists
 
-        if(this.exists){
-            
+        this.data = this.loadFromFile(this.path)
+        console.log(this.data)
+    }
+
+    private loadFromFile = (path:string):string => {
+        if(!this.fileExists(path)) {
+            return JSON.parse(this.writeFileData(path, {}, true))
+        }
+        return JSON.parse(readFileSync(path).toString())
+    }
+
+    private writeFileData(path:string, data:any, json:boolean):string{
+        if(json){
+            data = JSON.stringify(data)
+        }
+        writeFile(path, data, (error:any) => {
+            if(error) {
+                this.throwStoreException(error.message)
+            }
+        })
+        return data
+    }
+
+    private fileExists = (path:string | null) => {
+        if(path == null){
+            return false
+        }
+        try {
+            return existsSync(path)
+        } catch(exception) {
+            return false
         }
     }
 
     private throwStoreException = (message:string) => {
         console.error(message)
         process.exit()
+    }
+
+    public joinServer = (serverId:string):void => {
+        this.data[serverId.toString()] 
     }
 
 }
---
+++ tests/store/test.ts
@@ -0,0 +1,9 @@
+import { join } from "path";
+import { cwd } from "process";
+import { SphinxDataStore } from "../../src/store/store";
+
+const data = new SphinxDataStore({
+    databaseName : "hello",
+    databasePath : join(cwd(), "test.json"),
+    exists : false
+})
